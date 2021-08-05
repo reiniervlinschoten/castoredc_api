ef0bd4eb8ac2efaa7e98e8462de7e5a7aa65a7f0
Reinier van Linschoten <reinier.v.linschoten@gmail.com>
2021-08-05 17:18:55 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,22 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="castoredc_api_client",
-    version="1.0.5",
+    name="castoredc_api",
+    version="0.0.1dev",
     description="Python wrapper for the Castor EDC API",
     author="Reinier van Linschoten",
     author_email="mail@reiniervl.com",
-    url="https://github.com/reiniervlinschoten/castoredc_api_client",
+    url="https://github.com/reiniervlinschoten/castoredc_api",
     packages=find_packages(
         exclude=("tests", "tests.*", "scripts", "scripts.*", "auth", "auth.*")
     ),
     install_requires=[
+        "pandas",
+        "numpy",
+        "openpyxl",
+        "tqdm",
         "requests",
+        "pyarrow",
     ],
     license="MIT",
     long_description=long_description,
