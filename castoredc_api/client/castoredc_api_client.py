9bba93c53dc342492b52336e70ea9521199e656c
Reinier van Linschoten <reinier.v.linschoten@gmail.com>
2022-08-25 16:24:35 +0200

    Added recognizable user agent and made auth work with same client

--- castoredc_api/client/castoredc_api_client.py
+++ castoredc_api/client/castoredc_api_client.py
@@ -2,6 +2,7 @@
 
 import asyncio
 import csv
+import json
 import sys
 from datetime import datetime
 from itertools import chain
@@ -988,7 +989,7 @@ class CastorClient:
             "client_secret": client_secret,
             "grant_type": "client_credentials",
         }
-        response = self.client.post(url=self.auth_url, data=auth_data)
+        response = self.client.post(url=self.auth_url, data=json.dumps(auth_data))
         response.raise_for_status()
         content = response.json()
         return content["access_token"]
