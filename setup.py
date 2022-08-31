34b5598899d0efa5532eb39242d125a9c2022803
Reinier van Linschoten <reinier.v.linschoten@gmail.com>
2022-08-31 09:52:53 +0200

    Version of openpyxl downgraded to stay in sync with conda forge

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="castoredc_api",
-    version="0.1.5-2",
+    version="0.1.5-3",
     description="Python wrapper for the Castor EDC API",
     author="Reinier van Linschoten",
     author_email="mail@reiniervl.com",
@@ -16,7 +16,7 @@ setup(
     install_requires=[
         "pandas>=1.4.3",
         "numpy>=1.23.2",
-        "openpyxl>=3.0.10",
+        "openpyxl>=3.0.9",
         "tqdm>=4.64.0",
         "httpx>=0.23.0",
         # importlib.metadata was only introduced in Python 3.8, but the
